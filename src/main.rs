3ed07af3767cbf02172ab62b47c5cffc77f0a0cc
xnuray98s <66944976+xnuray98s@users.noreply.github.com>
2022-05-25 05:55:06 +0300

    removing unused variables

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ use std::env;
 use std::process;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
     let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
